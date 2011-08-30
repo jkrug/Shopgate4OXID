59ecf64cafc0e9a4af2548072d80042be14e005d
Mazvydas Skuodas <wanis1@gmail.com>
2011-08-30 11:09:11 +0300

    oxid plugin for shopgate interface, and directory infrastructure for framework (copy framework files here, do not overwrite .htaccess)

---
+++ eshop/shopgate/plugins/plugin_oxid.inc.php
@@ -0,0 +1,37 @@
+<?php
+
+
+class ShopgatePlugin extends ShopgatePluginCore {
+    
+    public function startup() 
+    {
+        return true;
+    }
+    
+    protected function createItemsCsv()
+    {
+
+    }
+
+    protected function createReviewsCsv()
+    {
+
+    }
+    
+    protected function createPagesCsv()
+    {
+
+    }
+    
+    public function getUserData($user, $pass)
+    {
+
+    }
+    
+    public function createShopInfo(){}
+
+    public function saveOrder(ShopgateOrder $order)
+    {
+        
+    }
+}
