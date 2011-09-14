d7e5fe2b771f3ed665bfd15d7ae426f857eb3082
Mazvydas Skuodas <wanis1@gmail.com>
2011-09-14 16:14:48 +0300

    marm_shopgate PHPUnit tests for marm_shopgate_article

--- unittests/unit/marm_shopgate/admin/marm_shopgate_articleTest.php
+++ unittests/unit/marm_shopgate/admin/marm_shopgate_articleTest.php
@@ -6,4 +6,124 @@ require_once 'unit/test_config.inc.php';
 
 class unit_marm_shopgate_admin_marm_shopgate_articleTest extends OxidTestCase
 {
+    protected function tearDown()
+    {
+        modInstances::cleanup();
+        oxTestModules::cleanUp();
+    }
+
+    public function test_getArticle()
+    {
+        $sFirstId = 'superid';
+        $oFirstObject = new stdClass();
+        $oFirstObject->someValue = 'first data';
+        $sSecondId = 'duperid';
+        $oSecondObject = new stdClass();
+        $oSecondObject->anotherValue = 'other info';
+        $oView = $this->getMock(
+            'marm_shopgate_article',
+            array(
+                 'getEditObjectId'
+            )
+        );
+        $oView
+            ->expects($this->at(0))
+            ->method('getEditObjectId')
+            ->will($this->returnValue($sFirstId))
+        ;
+        $oView
+            ->expects($this->at(1))
+            ->method('getEditObjectId')
+            ->will($this->returnValue($sSecondId))
+        ;
+
+        $oModUtlisObject = $this->getMock(
+            'oxUtilsObject',
+            array(
+                'oxNewArticle'
+            )
+        );
+        $oModUtlisObject
+            ->expects($this->at(0))
+            ->method('oxNewArticle')
+            ->with($sFirstId)
+            ->will($this->returnValue($oFirstObject))
+        ;
+        $oModUtlisObject
+            ->expects($this->at(1))
+            ->method('oxNewArticle')
+            ->with($sSecondId)
+            ->will($this->returnValue($oSecondObject))
+        ;
+
+        modInstances::addMod('oxUtilsObject', $oModUtlisObject);
+        $this->assertEquals($oFirstObject, $oView->getArticle());
+        $this->assertEquals($oFirstObject, $oView->getArticle());
+        $this->assertEquals($oSecondObject, $oView->getArticle(true));
+    }
+
+    public function test_save()
+    {
+        $aRequestData = array(
+            'firstkey' => 'firstvalue',
+            'seckey' => 'secVal'
+        );
+        $sSavingId = 'first';
+        $sAfterSaveId = 'second';
+
+        modConfig::setParameter('editval',$aRequestData);
+
+        $oView = $this->getMock(
+            'marm_shopgate_article',
+            array(
+                'getEditObjectId',
+                'setEditObjectId'
+            )
+        );
+        $oView
+            ->expects($this->once())
+            ->method('getEditObjectId')
+            ->will($this->returnValue($sSavingId))
+        ;
+        $oView
+            ->expects($this->once())
+            ->method('setEditObjectId')
+            ->with($sAfterSaveId)
+        ;
+        $oNewArticle = $this->getMock(
+            'oxArticle',
+            array(
+                'setLanguage',
+                'loadInLang',
+                'assign',
+                'save',
+                'getId'
+            )
+        );
+        $oNewArticle
+            ->expects($this->exactly(3))
+            ->method('setLanguage')
+        ;
+        $oNewArticle
+            ->expects($this->once())
+            ->method('loadInLang')
+        ;
+        $oNewArticle
+            ->expects($this->once())
+            ->method('assign')
+            ->with($aRequestData);
+        ;
+        $oNewArticle
+            ->expects($this->once())
+            ->method('save')
+        ;
+        $oNewArticle
+            ->expects($this->once())
+            ->method('getId')
+            ->will($this->returnValue($sAfterSaveId))
+        ;
+        oxTestModules::addModuleObject('oxArticle', $oNewArticle);
+        
+        $oView->save();
+    }
 }
