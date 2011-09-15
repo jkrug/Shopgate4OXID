0699abe4301de3a00cd06b17c71228af37b3d809
Mazvydas Skuodas <wanis1@gmail.com>
2011-09-15 13:34:27 +0300

    marm_shopgate PHPUnit added fake ShopgateFramework class and fake ShopgateConfig with static functions setConfig and getConfig

--- unittests/unit/marm_shopgate/shopgate/plugins/plugin_oxidTest.php
+++ unittests/unit/marm_shopgate/shopgate/plugins/plugin_oxidTest.php
@@ -7,6 +7,35 @@ require_once 'unit/test_config.inc.php';
  * Dummy class for testing only
  */
 class ShopgatePluginCore {}
+class ShopgateFramework {}
+class ShopgateConfig {
+    static public function setConfig(){}
+    static public function getConfig(){
+        return array(
+		'api_url' => 'https://api.shopgate.com/shopgateway/api/',
+		'customer_number' => 'THE_CUSTOMER_NUMBER',
+		'shop_number' => 'THE_SHOP_NUMBER',
+		'apikey' => 'THE_API_KEY',
+		'server' => 'live',
+		'plugin' => '',
+		'plugin_language' => 'DE',
+		'plugin_currency' => 'EUR',
+		'plugin_root_dir' => "",
+		'enable_ping' => true,
+		'enable_get_shop_info' => true,
+		'enable_http_alert' => true,
+		'enable_connect' => true,
+		'enable_get_items_csv' => true,
+		'enable_get_reviews_csv' => true,
+		'enable_get_pages_csv' => true,
+		'enable_get_log_file' => true,
+		'enable_mobile_website' => true,
+		'generate_items_csv_on_the_fly' => true,
+		'max_attributes' => 50,
+		'use_custom_error_handler' => false,
+	);
+    }
+}
 require_once getShopBasePath() . 'shopgate/plugins/plugin_oxid.inc.php';
 
 
